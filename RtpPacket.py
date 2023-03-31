06484fb6d723a616e85ae2fe400cec94040da7bc
Minh Nhật Mai <nhat.maik20dhbk@hcmut.edu.vn>
2023-03-31 07:48:02 +0700

    Finished RtpPacket.py encode() function

--- RtpPacket.py
+++ RtpPacket.py
@@ -11,7 +11,7 @@ class RtpPacket:
 	def encode(self, version, padding, extension, cc, seqnum, marker, pt, ssrc, payload):
 		"""Encode the RTP packet with header fields and payload."""
 		timestamp = int(time())
-		header = bytearray(HEADER_SIZE)
+		self.header = bytearray(HEADER_SIZE)
 		#--------------
 		# TO COMPLETE
 		#--------------
@@ -19,9 +19,30 @@ class RtpPacket:
 		
 		# header[0] = ...
 		# ...
+		self.header[0] = self.header[0] | version << 6
+		self.header[0] = self.header[0] | padding << 5
+		self.header[0] = self.header[0] | extension << 4
+		self.header[0] = self.header[0] | cc
+		self.header[1] = self.header[1] | marker << 7
+		self.header[1] = self.header[1] | pt
+  
+		self.header[2] = (seqnum >> 8) & 0xFF
+		self.header[3] = seqnum & 0xFF
+
+		self.header[4] = (timestamp >> 24) & 0xFF
+		self.header[5] = (timestamp >> 16) & 0xFF
+		self.header[6] = (timestamp >> 8) & 0xFF
+		self.header[7] = timestamp & 0xFF
+  
+		self.header[8] = (ssrc >> 24) & 0xFF
+		self.header[9] = (ssrc >> 16) & 0xFF
+		self.header[10] = (ssrc >> 8) & 0xFF
+		self.header[11] = ssrc & 0xFF
 		
+  
 		# Get the payload from the argument
 		# self.payload = ...
+		self.payload = payload
 		
 	def decode(self, byteStream):
 		"""Decode the RTP packet."""
